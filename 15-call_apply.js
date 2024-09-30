cb125cae76c8ea79c22d61b5be5c1a4d1d139981
fluffybear007 <yyao2011@yahoo.com>
2024-09-30 15:02:52 -0500

    call_apply

--- 15-call_apply.js
+++ 15-call_apply.js
@@ -3,48 +3,69 @@
 // both call and apply - immedidately execute, change the context and return value
 // only defference between call and apply is - apply accepts parameters in the from of array
 
-var User1 = {name : "Michael", age : 18, address : "Somewhere in Mauritius" }
-var User2 = {name : "Mark", age: 19, address: "Somewher in France"}
 
-function PrintInfo(heading) {
-    // var name = "Ben Ma"
-    // var age = "21"      //window.age
-    // var address = "Somewhere in Italy" //window.address
+//1.
+var User1 = {name : "Michael", age : 18, address : "Somewhere in Mauritius" }//object
+var User2 = {name : "Mark", age: 19, address: "Somewher in France"}//object
+
+function PrintInfo() {
+
     console.log(this) // this - is refered as dynamic context and is changeable as per the context setting 
     console.log(`
-        ${this.heading}
-        ${this.name}
+      
+        ${this.name} 
         ${this.age}
         ${this.address}
         `
     )
 }
 
-name = "Ben Ma" //window.name
-age = "21"      //window.age
-address = "Somewhere in Italy" //window.address
-
-PrintInfo("no contect set, default context is global"); // if we don't provide any context to the function it takes global object as its context: <window>
-
-PrintInfo.call(User1, "Setting contect to User1") // PrintInfo wil have User1 as its context
-PrintInfo.call(User2, "Setting contect to User2") // PrintInfo wil have User1 as its context
-
-// UserInfo usrObj = new UserInfo();
-// usrObj.PrintInfo(); //this
+// 1.executing the function, there is no varaible, object provide
+//PrintInfo() 
 
 
-function PrintSessionInfo(heading,  session1, session2, session3, session4, session5,) {
+//if we don't provide any context to the function it takes global object as its context
+
+//instance means reference to the dynamic context of this class
+// UserInfo usrObj = new UserInfo(); UserInfo have a limitation { }
+// usrObj.PrintInfo(); //this  means useObj, in the class scope
+
+
+
+ //2.
+// name = "Ben Ma" //window.name  glocal variable no keyword 
+// age = "21"      //window.age   
+// address = "Somewhere in Italy" //window.address
+// PrintInfo()
+// PrintInfo("no contect set, default context is global"); // if we don't provide any context to the function it takes global object as its context: <window>
+
+//3.
+// PrintInfo.call(User1, "Setting contect to User1") // PrintInfo wil have User1 as its context
+// PrintInfo.call(User2, "Setting contect to User2") // PrintInfo wil have User1 as its context
+
+
+
+//4.
+//  function PrintSessionInfo(heading,name,age, address) {
     
-    console.log(this) // this - is refered as dynamic context and is changeable as per the context setting 
-    console.log(`
-        ${this.heading}
-        ${this.name}
-        ${this.age}
-        ${this.address}
-        `
-    )
+//     console.log(this) // this - is refered as dynamic context and is changeable as per the context setting 
+//     console.log(`
+//         ${this.heading}
+//         ${this.name}
+//         ${this.age}
+//         ${this.address}
+//         `
+//     )
+// }
 
-    console.log(`
+// PrintSessionInfo.call(User1, "Context is USer1", "First", "Mernstack", "DSA", "JAVA", "AWS")
+
+
+//5. 
+function PrintSessionInfo2(heading,session1, session2, session3, session4, session5){
+    console.log(this)
+    console.log(
+        `
         Session Details are below:
         ${session1}
         ${session2}
@@ -55,19 +76,19 @@ function PrintSessionInfo(heading,  session1, session2, session3, session4, sess
     )
 }
 
+// PrintSessionInfo2.call(User1, "Context is USer1", "First", "Mernstack", "DSA", "JAVA", "AWS")
 
-var sessionListSept = ["Context is USer1", "First", "Mernstack", "DSA", "JAVA", "AWS"]
-var sessionListOct = ["Context is USer1", "Algo", "Mernstack", "DSA", "JAVA", "AWS"]
+//6. apply
 
-
-PrintSessionInfo.call(User1, "Context is USer1", "First", "Mernstack", "DSA", "JAVA", "AWS")
+var sessionListSept = ["Context is USer1", "First", "Mernstack", "DSA", "JAVA", "AWS1"]
+var sessionListOct = ["Context is USer1", "Algo", "Mernstack", "DSA", "JAVA", "AWS2"]
 
 //Apply takes function parameters as array and applies the context same way we do with call
-PrintSessionInfo.apply(User1, sessionListSept)
-PrintSessionInfo.apply(User2, sessionListSept)
+// PrintSessionInfo2.apply(User1, sessionListSept)
+// PrintSessionInfo2.apply(User2, sessionListOct)
 
-PrintSessionInfo.apply(null, sessionListSept)
-PrintSessionInfo.apply(null, sessionListOct)
+PrintSessionInfo2.apply(null, sessionListSept)
+PrintSessionInfo2.apply(null, sessionListOct)
 
 
 
--- 16-bind.js
+++ 16-bind.js
@@ -1,9 +1,47 @@
+// Create two examples 
+// Bind : is the function present with each function which is used to change the context or hold the context
+// for the time function gets executed
+// in other words this is similar to call and apply but doesn't execute the function immedidately.
+// however becomes part of the same function by holding the new context supplied - Same way as parents or v
 
 
 for(var index =0; index < 5; index ++){
+    //setTimeout excecutes the call back function passed as first parameter 
+    //after the milliseconds passed as second param - for one time only
     setTimeout(function(){
-        console.log(index)
-    }, 1000);
+        console.log(index)//setTimeout excecutes the call back function passed as first parameter 
+
+},1000);
 }
 
 console.log(index)
+
+var User = {
+    name : "Yao",
+    age : 18,
+    address : "Somewhere in China",
+    GetUserDetails : function () {
+        console.log(this)
+        console.log(
+            `
+            ${this.name}
+            ${this.age}
+            ${this.address}
+            `
+        )
+
+        setTimeout(function(){
+            console.log(this)
+            console.log(
+                `
+                ${this.name}
+                ${this.age}
+                ${this.address}
+                `
+            )
+    
+    },2000);
+    }
+
+}
+User.GetUserDetails()
