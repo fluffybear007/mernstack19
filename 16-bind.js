9c8a5c3e59f64bdc313157fa36590432f7c1b1ba
fluffybear007 <yyao2011@yahoo.com>
2024-10-02 22:26:26 -0500

    assignment

--- 16-bind.js
+++ 16-bind.js
@@ -1,20 +1,22 @@
 // Create two examples 
-// Bind : is the function present with each function which is used to change the context or hold the context
+// bind : is the function present with each function which is used to change the context or hold the context
 // for the time function gets executed
 // in other words this is similar to call and apply but doesn't execute the function immedidately.
-// however becomes part of the same function by holding the new context supplied - Same way as parents or v
+// however becomes part of the same function by holding the new context supplied 
+// example: Same way as parents or caetakers hold the money and give it to us when we need
 
+// var index =0
+// for(var index =0; index < 5; index ++){
+//     //setTimeout is registered API, it set the function(parameter) every 2s to execute, only one function to execute with timer
+//     //setTimeout excecutes the call back function passed as first parameter 
+//     //after the milliseconds passed as second param - for one time only
+//     //setTimeout execute 5 times 
+//     setTimeout(function(){
+//         console.log(index)//function inside is callback function of setTimeout.
+//     },1000)
+// }
 
-for(var index =0; index < 5; index ++){
-    //setTimeout excecutes the call back function passed as first parameter 
-    //after the milliseconds passed as second param - for one time only
-    setTimeout(function(){
-        console.log(index)//setTimeout excecutes the call back function passed as first parameter 
-
-},1000);
-}
-
-console.log(index)
+// console.log(index)
 
 var User = {
     name : "Yao",
@@ -29,7 +31,7 @@ var User = {
             ${this.address}
             `
         )
-
+        //there is no context for function, so it use setTimeout context--Timeout object
         setTimeout(function(){
             console.log(this)
             console.log(
@@ -37,11 +39,9 @@ var User = {
                 ${this.name}
                 ${this.age}
                 ${this.address}
-                `
-            )
-    
-    },2000);
+                `)
+            //return "Yao"
+    }.bind(User),2000);
     }
-
 }
 User.GetUserDetails()
